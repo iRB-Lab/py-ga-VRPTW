fdd92341e89a2bcad6f2beb03ddb3ad1f60f689b
iRB Air <iROCKBUNNY@users.noreply.github.com>
2016-05-17 04:50:49 +0800

    Update

---
+++ txt2json.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import json
+from basic.common import ROOT_PATH, makeDirsForFile, existFile
+
+reload(sys)
+sys.setdefaultencoding('utf8');
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
